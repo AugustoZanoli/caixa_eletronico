948d46305f6f493433de16c4ba8608e0b9d4a7a1
Augusto <zancamar9@gmail.com>
2023-10-17 14:15:25 -0300

    "Criação da função sacar"

--- contas.dart
+++ contas.dart
@@ -21,6 +21,33 @@ class ContaBancaria {
 
       default:
         print('Finalizando...\nFinalizado!');
+        break;
+    }
+  }
+
+  //Criação da função de sacar
+  bool sacarDinheiro(double valor) {
+    if (saldo >= valor) {
+      saldo -= valor;
+      print(
+          'Saque realizado!\nValor de saque:${valor}\nDeseja ver o novo saldo?\n1- sim\n2- não');
+
+      var opcao = int.parse(stdin.readLineSync().toString());
+
+      switch (opcao) {
+        case 1:
+          print('Novo saldo: $saldo');
+          break;
+
+        default:
+          print('Finalizando...\nFinalizado!');
+          break;
+      }
+
+      return true;
+    } else {
+      print('Saldo insuficiente para saque.');
+      return false;
     }
   }
 }
