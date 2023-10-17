a24b44b39d277f969faf4b1c0c2a6597a77784e3
Augusto <zancamar9@gmail.com>
2023-10-17 14:08:03 -0300

    "Primeira função criada"

--- contas.dart
+++ contas.dart
@@ -1,7 +1,26 @@
+import 'dart:io';
+
 class ContaBancaria {
   String titular;
   var conta;
   double saldo;
 
   ContaBancaria(this.titular, this.conta, this.saldo);
+
+  //Criação da função depositar dinheiro
+  void depositarDinheiro(double valor) {
+    saldo += valor;
+    print('Depósito realizado! Deseja ver o novo saldo?\n1- sim\n2- não');
+
+    var opcao = int.parse(stdin.readLineSync().toString());
+
+    switch (opcao) {
+      case 1:
+        print('Novo saldo: $saldo');
+        break;
+
+      default:
+        print('Finalizando...\nFinalizado!');
+    }
+  }
 }
